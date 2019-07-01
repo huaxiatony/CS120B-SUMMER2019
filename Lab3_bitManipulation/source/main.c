b0528b68d0c92e6dcac6f22c2b08a8caada272a7
XIA HUA <xhua006@wch136-18.cs.ucr.edu>
2019-06-30 20:58:52 -0700

    Add files

--- Lab3_bitManipulation/source/main.c
+++ Lab3_bitManipulation/source/main.c
@@ -11,31 +11,78 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-enum dumbMachine{INIT,LOCK,PAUSE,WAIT,} state;
+enum dumbMachine{INIT,LOCK,PAUSE,WAIT,KEY_Y,KEY_POUND} state;
 //0x01 PRESSED  0x00 NOT PRESSED
 //
 //
 
 void Tick(){
+	PORTC = 0x00;
+	PORTB = 0x02;
 	switch(state){
-		case INIT:{
-		
-		}
-		case  
-		
+		case INIT:
+			PORTC = 0x01;
+			if((PINA & 0x04) == 0x04){
+				state = KEY_PUND;
+				break;
+			}
+			else if((PINA & 0x80) == 0x80){
+				state = LOCK; 
+				break;
+			}
+			else{
+				state = INIT;
+			}
+		case WAIT:
+			PORTC = 0x02;
+			if((PINA & 0x02) == 0x02){
+				state = KEY_Y;
+				break;
+			}
+			else if((PINA & 0x80) == 0x80){
+				state = LOCK;
+				break;
+			}
+			else if((PINA & 0x01) == 0x01){
+				state = INIT;	
+				break;
+			}
+			else{
+				state = WAIT;
+				break;
+			}
+		case KEY_POUND:
+			PORTC = 0x03;
+			state = WAIT;
+			break;
+		case KEY_Y:
+			PORTC = 0x04;
+			PORTB = 0x01;
+			state = PAUSE;
+			break;
+		case PAUSE:
+			if((PINA & 0x87) == 0x00){
+				state = INIT;
+				break;
+			}
+			else{
+				state = PAUSE;	
+				break;
+			}
 
-		case
-		case
-
-		case
-		case
-
-
-
-
-
+			break;
+		case LOCK:
+			if((PINA & 0x00) == 0x00){
+				state = INIT;
+				break;
+			}
+			else{
+				state = LOCK;
+				break;
+			}
+		default:
+			break;
 }
-
 
 
 int main(void){
