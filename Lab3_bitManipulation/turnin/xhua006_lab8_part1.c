8937b6ddaec109b5529b333515cbe29a897ea969
XIA HUA <xhua006@wch136-09.cs.ucr.edu>
2019-07-09 14:17:46 -0700

    Lab8

--- Lab3_bitManipulation/turnin/xhua006_lab8_part1.c
+++ Lab3_bitManipulation/turnin/xhua006_lab8_part1.c
@@ -22,58 +22,17 @@ void ADC_init() {
 	// in Free Running Mode, a new conversion will trigger
 	// whenever the previous conversion completes.
 }
-unsigned short max = 0x00;
 unsigned short x = 0x00;
-unsigned short y = 0x00;
 int main(void)
 {
 	DDRA = 0x00;PORTA = 0xFF;
 	DDRB = 0xFF;PORTB = 0x00;
 	//DDRC = 0xFF; PORTC = 0x00;
-	//DDRD = 0xFF; PORTD = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
 	ADC_init();
 	while(1){
-		x = ADC;
-		if(x > max)
-		{
-			max = x;
-			y = (max/8); 
-		}
-		else if(x <= y)
-		{
-			PORTB = 0x00;
-		}
-		else if(x <= (2*y))
-		{
-			PORTB = 0x01;
-		}
-		else if(x <= (3*y))
-		{
-			PORTB = 0x03;
-		}
-		else if(x <= (4*y))
-		{
-			PORTB = 0x07;
-		}
-		else if(x <= (5*y))
-		{
-			PORTB = 0x0F;
-		}
-		else if(x <= (6*y))
-		{
-			PORTB = 0x1F;
-		}
-		else if(x <= (7*y))
-		{
-			PORTB = 0x3F;
-		}
-		else if(x <= (8*y))
-		{
-			PORTB = 0x7F; 
-		}
-		else
-		{
-			PORTB = 0xFF;
-		}
+	x = ADC;
+	PORTB = (char)x;
+	PORTD = (char)(x >> 8);
 	}
 }
--- Lab3_bitManipulation/turnin/xhua006_lab8_part3.c
+++ Lab3_bitManipulation/turnin/xhua006_lab8_part3.c
@@ -24,7 +24,6 @@ void ADC_init() {
 }
 unsigned short max = 0x00;
 unsigned short x = 0x00;
-unsigned short y = 0x00;
 int main(void)
 {
 	DDRA = 0x00;PORTA = 0xFF;
@@ -32,48 +31,23 @@ int main(void)
 	//DDRC = 0xFF; PORTC = 0x00;
 	//DDRD = 0xFF; PORTD = 0x00;
 	ADC_init();
+	
 	while(1){
 		x = ADC;
-		if(x > max)
-		{
+		if(x > max){
 			max = x;
-			y = (max/8); 
 		}
-		else if(x <= y)
-		{
-			PORTB = 0x00;
-		}
-		else if(x <= (2*y))
-		{
+		else if(x >= (max/2)){
 			PORTB = 0x01;
-		}
-		else if(x <= (3*y))
-		{
-			PORTB = 0x03;
-		}
-		else if(x <= (4*y))
-		{
-			PORTB = 0x07;
-		}
-		else if(x <= (5*y))
-		{
-			PORTB = 0x0F;
-		}
-		else if(x <= (6*y))
-		{
-			PORTB = 0x1F;
-		}
-		else if(x <= (7*y))
-		{
-			PORTB = 0x3F;
-		}
-		else if(x <= (8*y))
-		{
-			PORTB = 0x7F; 
 		}
 		else
 		{
-			PORTB = 0xFF;
-		}
+			PORTB = 0x00;
+		}	
+	/*
+	x = ADC;
+	PORTB = (char)x;
+	PORTD = (char)(x >> 8);
+	*/
 	}
 }
